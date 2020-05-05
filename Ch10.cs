8186b7a57539d5ae3410fca1fe7230db3583d273
kimjihoon <21qqq4@gmail.com>
2020-05-06 00:32:28 +0900

    chapter 10, 12

---
+++ Ch10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace Brain_Cs
+{
+   
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht["company"] = "Microsoft";
+            ht["URL"] = "www.microsoft.com";
+
+            Console.WriteLine("회사 : {0}", ht["company"]);
+            Console.WriteLine("URL : {0}", ht["URL"]);
+        }
+    }
+
+}
---
+++ Ch12.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Brain_Cs
+{
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            for (int i =0; i < 10; i++)
+            {
+                arr[i] = i;
+            }
+
+            try
+            {
+                for (int i = 0; i < 11; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                    if(arr[i] == null)
+                    {
+                        throw new Exception();
+                    }
+                }
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine("인덱스가 배열 범위를 벗어났습니다.");
+            }
+        }
+    }
+
+}
